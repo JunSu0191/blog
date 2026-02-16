6f6c41b875d2d04233fd9ea8495b48c34b4105b0
project-maintainer <maintainer@example.com>
2026-02-16 19:06:28 +0900

    chore(test): 테스트 기본 설정을 H2로 전환한다

--- src/test/java/com/study/blog/BlogApplicationTests.java
+++ src/test/java/com/study/blog/BlogApplicationTests.java
@@ -3,7 +3,7 @@ package com.study.blog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.flyway.enabled=false")
 class BlogApplicationTests {
 
 	@Test
